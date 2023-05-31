ef06b882daa65e587547a7fdf7c13908c949434b
Meghashree N <meghashreetendulkar@gmail.com>
2023-05-31 12:08:44 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,19 +2,19 @@ provider "oci" {
   alias            = "home"
   version          = ">=3.11"
   tenancy_ocid     = var.tenancy_ocid
-  region           = var.region
+  region           = var.home_region
 }
 
 provider "oci" {
   alias            = "region1"
   version          = ">=3.11"
   tenancy_ocid     = var.tenancy_ocid
-  region           = var.region
+  region           = var.region1
 }
 
 provider "oci" {
   alias            = "region2"
   version          = ">=3.11"
   tenancy_ocid     = var.tenancy_ocid
-  region           = var.region
+  region           = var.region2
 }
